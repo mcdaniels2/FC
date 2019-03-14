b612f1100bc3bd91dde4f81f4c8db8eb15fb9914
Molly <mcdaniels2@wisc.edu>
2019-03-13 19:09:20 -0500

    Updated Year_Built and Dam_Height code, saved histograms

--- dam_removal_script.R
+++ dam_removal_script.R
@@ -24,7 +24,7 @@ str(damrem) #note year is a factor
 damrem$Year_Removed <- as.character(damrem$Year_Removed)
 damrem$Year_Removed <- as.numeric(damrem$Year_Removed)
 
-# now to plot
+##### plotting dams over time #####
 #plotting first question as bar graph
 plotdamsovertime <- ggplot(damrem, aes(Year_Removed, count)) +
   geom_bar(stat = "identity") + theme_classic(base_size = 16) +
@@ -86,82 +86,52 @@ dambuilt <- ddply(ddata, c("Year_Built"), summarise,
                 count = length(Year_Built))
 dambuilt
 head(dambuilt, n=10) 
-# looks like we have blanks
-dambuilt <- dambuilt[-1,] 
-# blanks are gone, but a lot of approx.'s (i.e. ~1900, ~1700)
 
 #adjust the way year is coded in the dataframe
-str(dambuilt) #note year_built is a factor
-dambuilt$Year_Built <- as.character(dambuilt$Year_Built)
-dambuilt$Year_Built <- as.numeric(dambuilt$Year_Built)
+ddata$Year_Built <- as.character(ddata$Year_Built)
+ddata$Year_Built <- as.numeric(ddata$Year_Built)
 
-summary(dambuilt)
+# now for summary stats:
+mean(ddata$Year_Built, na.rm = TRUE) # mean provided is 1916.002
+min(ddata$Year_Built, na.rm = TRUE)
+max(ddata$Year_Built, na.rm = TRUE)
+median(ddata$Year_Built, na.rm = TRUE)
 
 
 # now to plot
-
-plotdamsbuilt <- ggplot(dambuilt, aes(Year_Built, count)) +
-  geom_line(stat = "identity") + theme_classic() +
-  scale_x_continuous(breaks = c(seq(1900,2020, by = 50))) +
-  labs(y = "Dams Built", x = "Year")
-plotdamsbuilt
-#looks like a nearly flat line...
-
-plotdamsbuilt2 <- ggplot(dambuilt, aes(Year_Built, count)) +
-  geom_line(stat = "identity") + theme_classic() +
-  scale_x_continuous(limits = c(1720, 2000)) +
-  scale_y_continuous(limits = c(0,20)) +
-  labs(y = "Dams Built", x = "Year")
-plotdamsbuilt2
-
-plotdamsbuilt3 <- ggplot(dambuilt, aes(Year_Built, count)) +
-  geom_point(stat = "identity", shape = 18, size = 1.5) + theme_classic() +
-  geom_smooth() +
-  scale_x_continuous(limits = c(1720, 2000)) +
-  scale_y_continuous(limits = c(0,15)) +
-  labs(y = "Dams Built", x = "Year")
-plotdamsbuilt3
-
-# now a histogram
-hist(dambuilt$Year_Built)
-
-# adding a meanline for year built
-#determining mean year built
-mean(dambuilt$Year_Built, na.rm = TRUE)
-# 1900.32
-#adding line to histogram:
-abline(v=1900.32,col="red")
-
-# now for summary stats:
-summary(dambuilt$Year_Built)
+plotdamsbuilt3 <- ggplot(ddata, aes(Year_Built)) +
+  geom_histogram(size = 1.5, bins = 50, aes(fill = "orange")) + 
+  theme_classic() +
+  geom_vline(xintercept = 1916.002, size = 1) + 
+  labs(y = "Dam Count", x = "Year Built")
+plotdamsbuilt3 + theme(legend.position="none")
 
 ###
 
 ##### dam heights #####
+
 # make sure dam height is numeric:
+ddata$Dam_Height_ft<- as.character(ddata$Dam_Height_ft)
 ddata$Dam_Height_ft<- as.numeric(ddata$Dam_Height_ft)
-#create histogram:
-hist(ddata$Dam_Height_ft)
-# calculate stats:
-mean(ddata$Dam_Height_ft, na.rm = TRUE)
-# 50.727
-summary(ddata$Dam_Height_ft)
 
-# add mean line to histogram
-abline(v = 50.727, col = "red")
-
-# re-do with height in meters
-
+# create column with height in meters
 ddata %>%
   mutate(Dam_Height_m = conv_unit(Dam_Height_ft, "ft", "m")) -> ddata
 ddata$Dam_Height_m <- as.numeric(ddata$Dam_Height_m)
-mean(ddata$Dam_Height_m, na.rm = TRUE)
-# mean is 15.46m
-summary(ddata$Dam_Height_m)
 
-# histogram of heigh in meters
-hist(ddata$Dam_Height_m)
-abline(v = 15.46, col = "red")
+mean(ddata$Dam_Height_m, na.rm = TRUE) # 4.4234
+min(ddata$Dam_Height_m, na.rm = TRUE)
+max(ddata$Dam_Height_m, na.rm = TRUE)
+median(ddata$Dam_Height_m, na.rm = TRUE)
+
+
+#create histogram:
+plotdamheight <- ggplot(ddata, aes(Dam_Height_m)) +
+  geom_histogram(size = 1.5, bins = 50, fill = "purple") + 
+  theme_classic() +
+  geom_vline(xintercept = 4.42, size = 1) + 
+  labs(y = "Frequency", x = "Dam Height (meters)")
+plotdamheight + theme(legend.position="none")
 
 ###
 
@@ -171,6 +141,6 @@ abline(v = 15.46, col = "red")
 
 #ggsave("removal.by.state.pdf", plot = plotremovalbystate2, width = 8, height = 5, units = c("in"))
 
-#ggsave("built.trend.pdf", plot = plotdamsbuilt3, width = 7, height = 5, units = c("in"))
+#ggsave("built.hist3.pdf", plot = plotdamsbuilt3, width = 7, height = 5, units = c("in"))
 
 
